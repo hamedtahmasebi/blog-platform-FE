10c960699248472d9b1524ce363a79f99e43895d
Hamed tahmasebi <hamedse616@outlook.com>
2023-04-12 20:15:38 +0330

    Config the project

---
+++ src/components/Home/template/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <nav className="flex py-2 px-4 w-full">
+            <h3 className="text-2xl">Muse</h3>
+        </nav>
+    );
+};
