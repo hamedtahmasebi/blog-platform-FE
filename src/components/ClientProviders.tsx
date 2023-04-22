25f8ac1ef1f17076b7bfe3db2d73da55cd9db72c
Hamed tahmasebi <hamedse616@outlook.com>
2023-04-23 00:59:24 +0330

    Remove antd

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,14 +1,8 @@
-import { theme } from '@src/core/styles/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ConfigProvider } from 'antd';
 import { ReactNode } from 'react';
 
 const queryClient = new QueryClient();
 
 export const ClientProviders = ({ children }: { children: ReactNode }) => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ConfigProvider theme={theme}>{children}</ConfigProvider>
-        </QueryClientProvider>
-    );
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    corePlugins: {
-        preflight: false,
-    },
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}',
